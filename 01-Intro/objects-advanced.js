797ee88f0bf0e259e58857ea67f3c2f1b679b780
Beto Costa <robertomarx@gmail.com>
2019-05-02 14:50:07 -0300

    finished advanced objects

--- 01-Intro/objects-advanced.js
+++ 01-Intro/objects-advanced.js
@@ -21,11 +21,11 @@ INDEX - # Code Line
 149 - Getters
 198 - Setters
 256 - Factory Functions
-
-
-
+282 - Property Value Shorthand - Destructuring
+305 - Destructured Assignment
+340 - Built-in Object Methods
 */
-console.log('\n==Advanced Objects==\n');
+console.log('\n=== Advanced Objects ===\n');
 console.log('The \'this\' keyword:');
 // Using the this keyword on objects
 // We will create a function that we will use to create objects in a easier way:
@@ -59,7 +59,7 @@ goat.diet();
 The this keyword references the calling object which provides access to the calling object’s properties. In the example above, the calling object is goat and by using this we’re accessing the goat object itself, and then the dietType property of goat by using property dot notation.
 */
 
-console.log('\nArrow functions => and this:\n');
+console.log('\n=== Arrow functions => and this ===\n');
 /* Arrow Functions and 'this'
 const goat = {
   dietType: 'herbivore',
@@ -107,7 +107,7 @@ const smallrobot = {
 };
 smallrobot.checkEnergy();
 
-console.log('\nPrivacy\n');
+console.log('\n=== Privacy ===\n');
 /*
 Privacy
 Accessing and updating properties is fundamental in working with objects. However, there are cases in which we don’t want other code simply accessing and updating an object’s properties. When discussing privacy in objects, we define it as the idea that only certain properties should be mutable or able to change in value.
@@ -146,7 +146,7 @@ console.log('After we break it. The car energy level: ', electricCar._energyLeve
 /* If we change the _energyLevel above to something like a 'string', we would get unexpected
 results after using the recharge() function. That's why we should avoid to change it. */
 
-console.log('\nGetters:\n');
+console.log('\n=== Getters ===\n');
 /* Getters
 Getters are methods that get and return the internal properties of an object. But they can do more than just retrieve the value of a property!
 */
@@ -195,7 +195,7 @@ const weirdRobot = {
 console.log('Getting the Weird Robot Energy Level:')
 console.log(weirdRobot.energyLevel);
 
-console.log('\nSetters:\n');
+console.log('\n=== Setters ===\n');
 /* Setters
 Along with getter methods, we can also create setter methods which reassign values of existing properties within an object.
 */
@@ -253,5 +253,148 @@ console.log('the robot num of sensors after using set:');
 console.log(robot.numOfSensors);
 
 
-console.log('\n==Factory Functions==\n');
-/* Factory Functions */
+console.log('\n=== Factory Functions ===\n');
+/* Factory Functions
+So far we’ve been creating objects individually, but there are times where we want to create many instances of an object quickly. Here’s where factory functions come in. A real world factory manufactures multiple copies of an item quickly and on a massive scale. A factory function is a function that returns an object and can be reused to make multiple object instances. Factory functions can also have parameters allowing us to customize the object that gets returned.
+
+Let’s say we wanted to create an object to represent monsters in JavaScript. There are many different types of monsters and we could go about making each monster individually but we can also use a factory function to make our lives easier. To achieve this diabolical plan of creating multiple monsters objects, we can use a factory function that has parameters:
+*/
+const monsterFactory = (name, age, energySource, catchPhrase) => {
+  return {
+    name: name,
+    age: age,
+    energySource: energySource,
+    scare() {
+      console.log(catchPhrase);
+    }
+  }
+};
+
+/* In the monsterFactory function above, it has four parameters and returns an object that has the properties: name, age, energySource, and scare(). To make an object that represents a specific monster like a ghost, we can call monsterFactory with the necessary arguments and assign the return value to a variable */
+console.log('Monsters created using a factory function:')
+const gruntar = monsterFactory('gruntar', 150, 'food', 'I eat all the food!');
+const akratian = monsterFactory('akratian', 230, 'water', 'I\'ll kill you for that!');
+console.log(gruntar);
+gruntar.scare();
+console.log(akratian);
+akratian.scare();
+
+console.log('\n=== Property Value Shorthand - Destructuring ===\n');
+/* Property Value Shorthand
+ES6 introduced some new shortcuts for assigning properties to variables known as destructuring.
+
+In the previous exercise, we created a factory function that helped us create objects. We had to assign each property a key and value even though the key name was the same as the parameter name we assigned to it.
+
+Imagine if we had to include more properties, that process would quickly become tedious! But we can use a destructuring technique, called property value shorthand, to save ourselves some keystrokes.
+*/
+const betterMonsterFactory = (name, age, energySource, catchPhrase) => {
+  return {
+    name,
+    age,
+    energySource,
+    scare() {
+    console.log(catchPhrase);
+    }
+  }
+};
+console.log('New monster created in the new factory:');
+const mandragora = betterMonsterFactory('Mandragora', 45, 'air', 'Don\'t eat me!');
+console.log(mandragora);
+mandragora.scare();
+
+console.log('\n=== Destructured Assignment ===\n');
+/* Destructured Assignment
+We often want to extract key-value pairs from objects and save them as variables. Take for example the following object:*/
+const vampire = {
+  name: 'Dracula',
+  residence: 'Transylvania',
+  preferences: {
+    day: 'stay inside',
+    night: 'satisfy appetite'
+  }
+};
+console.log(vampire);
+/* we can also take advantage of a destructuring technique called destructured assignment to save ourselves some keystrokes. In destructured assignment we create a variable with the name of an object’s key that is wrapped in curly braces { } and assign to it the object. Take a look at the example below: */
+const { residence } = vampire;
+console.log('Destructured vampire residence:')
+console.log(residence);
+
+const { day } = vampire.preferences;
+console.log('What the vampire does in the day?');
+console.log(day);
+
+const monster = {
+  name: 'Gojira',
+  gsize: 80,
+  gweight: 4500
+}
+const { gsize, gweight} = monster;
+console.log('\n', monster);
+console.log(gsize);
+console.log(gweight);
+console.log('And what about the types?');
+console.log(`gsize variable that holds the Gojira weight:
+The type is: ${typeof gsize}`);
+
+
+console.log('\n=== Built-in Object Methods ===\n');
+/* Built-in Object Methods
+Built-in Object Methods
+In the previous exercises we’ve been creating instances of objects that have their own methods. But, we can also take advantage of built-in methods for Objects!
+
+For example, we have access to object instance methods like: .hasOwnProperty(), .valueOf(), and many more! Practice your documentation reading skills and check out: MDN’s object instance documentation.
+https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Object#Methods
+There are also useful Object class methods such as Object.assign(), Object.entries(), and Object.keys() just to name a few.
+*/
+
+const theRobot = {
+	model: 'SAL-1000',
+  mobile: true,
+  sentient: false,
+  armor: 'Steel-plated',
+  energyLevel: 75
+};
+
+// What is missing in the following method call?
+const theRobotKeys = Object.keys(theRobot);
+console.log('The Object.keys() method:');
+console.log(theRobotKeys);
+
+// Declare robotEntries below this line:
+const theRobotEntries = Object.entries(theRobot);
+console.log('The Object.entries() method:');
+console.log(theRobotEntries);
+
+// Declare newRobot below this line:
+// You can use the methods or key:value pairs to add and then the object, like below:
+const newRobot = Object.assign({laser: true, voiceRec: true}, theRobot);
+console.log('The copied and modified new robot:');
+console.log(newRobot);
+
+/* Review
+Congratulations on finishing Advanced Objects!
+
+Let’s review the concepts covered in this lesson:
+
+- The object that a method belongs to is called the calling object.
+- The this keyword refers the calling object and can be used to access properties of the calling object.
+- Methods do not automatically have access to other internal properties of the calling object.
+- The value of this depends on where the this is being accessed from.
+- We cannot use arrow functions as methods if we want to access other internal properties.
+- JavaScript objects do not have built-in privacy, rather there are conventions to follow to notify other developers about the intent of the code.
+- The usage of an underscore before a property name means that the original developer did not intend for that property to be directly changed.
+- Setters and getter methods allow for more detailed ways of accessing and assigning properties.
+- Factory functions allow us to create object instances quickly and repeatedly.
+- There are different ways to use object destructuring: one way is the property value shorthand and another is destructured assignment.
+- As with any concept, it is a good skill to learn how to use the documentation with objects!
+
+
+If you want to challenge yourself:
+
+Find the value of this in a function inside of a method.
+Learn the outcome of using a property that has the exact same name as a setter/getter method.
+Create a new factory function that can create object instances of your choice.
+Read documentation on other destructuring techniques and apply it to your code.
+Try out other built-in object methods and learn what they do.
+
+*/
