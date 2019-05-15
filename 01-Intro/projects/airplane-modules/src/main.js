04f1b612183e6d252a1a19b1fd238d9a319198ce
Beto Costa <robertomarx@gmail.com>
2019-05-15 17:04:57 -0300

    airplane project almost done

--- 01-Intro/projects/airplane-modules/src/main.js
+++ 01-Intro/projects/airplane-modules/src/main.js
@@ -1,4 +1,5 @@
 // import { airplanes, meetsRangeReq, meetsCrewReq } from './airplanes';
+// Import disable due to browser constraints for client mode.
 
 // The airplanes array
 const airplanes = [
@@ -30,9 +31,12 @@ const airplanes = [
   },
 ];
 
+// DOM Main global variables
+
 // Render the airplane list on the page:
 const renderList = (airplanes) => {
   // Getting the element sibling and removing the next sibling
+  // It is the first article for the airplanes GRID.
   let sibling = document.getElementsByClassName('airplane__functions')[0];
   sibling.parentElement.removeChild(sibling.nextElementSibling);
   // Creating the airplanes container
@@ -44,18 +48,19 @@ const renderList = (airplanes) => {
     let airplane = document.createElement('section');
     airplane.classList.add('airplane__container-airplane');
     let airplaneName = document.createElement('P');
+    airplaneName.setAttribute('data-airplanename', element.name);
     let airplaneFuelCapacity = document.createElement('P');
     let airplaneMaxSpeed = document.createElement('P');
     let airplaneMinSpeed = document.createElement('P');
     // Flight and Crew Requirements paragraphs
     let airplaneFlightReq = document.createElement('P');
     let airplaneCrewReq = document.createElement('P');
+    // FlightReq (range) span tags and data for checking
     let flightReqConfim = document.createElement('SPAN');
-    flightReqConfim.setAttribute('data-name', element.name);
-    // flightReqConfim.classList.add('airplane__flight-req');
+    flightReqConfim.setAttribute('data-flightname', element.name);
+    // crewReq span tags and data for checking
     let crewReqConfirm = document.createElement('SPAN');
-    crewReqConfirm.setAttribute('data-name', element.name);
-    // crewReqConfirm.classList.add('airplane__crew-req');
+    crewReqConfirm.setAttribute('data-crewname', element.name);
     // Handle the crew, put each one in a list-item
     let airplaneCrewContainer = document.createElement('UL');
     for (let i = 0; i < element['crew'].length; i++) {
@@ -69,10 +74,10 @@ const renderList = (airplanes) => {
     airplaneFuelCapacity.textContent = `Fuel Capacity: ${element.fuelCapacity}`;
     airplaneMaxSpeed.textContent = `Max Speed: ${element.maxSpeed}`;
     airplaneMinSpeed.textContent = `Min Speed: ${element.minSpeed}`;
-    airplaneFlightReq.textContent = `Flight Requirement meet: `;
-    airplaneCrewReq.textContent = `Crew Requirement meet: `;
-    flightReqConfim.textContent = `Placeholder`;
-    crewReqConfirm.textContent = `Placeholder`;
+    airplaneFlightReq.textContent = `Flight (range) Requirement meet: `;
+    airplaneCrewReq.textContent = `Crew (number) Requirement meet: `;
+    flightReqConfim.textContent = ``;
+    crewReqConfirm.textContent = ``;
     // Add the items to the container
     airplane.appendChild(airplaneName);
     airplane.appendChild(airplaneFuelCapacity);
@@ -106,18 +111,49 @@ const addAirplane = (name, fuelCapacity, maxSpeed, minSpeed, crew) =>  {
   renderList(airplanes);
 };
 
+// Get the airplane created by the user to run the add airplane function:
+const getUserAirplane = (e) => {
+  // Prevent form submit
+  e.preventDefault();
+  let name = document.getElementById('airplaneid').value;
+  let fuelCapacity = document.getElementById('fuelqtd').value;
+  let maxSpeed = document.getElementById('maxspd').value;
+  let minSpeed = document.getElementById('minspd').value;
+  let crewValue = document.getElementById('crew').value;
+  // Checking the values
+  if (name === '' || name === isNaN) {
+    return;
+  } else if (fuelCapacity === '' || fuelCapacity === isNaN) {
+    return;
+  } else if (maxSpeed === '' || maxSpeed === isNaN) {
+    return;
+  } else if (minSpeed === '' || minSpeed === isNaN) {
+    return;
+  } else if (crewValue === '') {
+    return
+  } else {
+    // Change the crew to an array and run the addAirplane function
+    let crew = crewValue.split(',');
+    addAirplane(name, fuelCapacity, maxSpeed, minSpeed, crew);
+  }
+}
+
+const addAirplaneBtn = document.getElementById('addairplanebtn');
+addAirplaneBtn.addEventListener('click', getUserAirplane);
+
 // Check if the airplane meets the crew requirements
 const meetsCrewReq = (crew) => {
   airplanes.forEach((airplane) => {
-    // Need to change here!
-    // Find a way to associate each airplane with it's own span tag inside the document.
-    // Probably using a data. Put data instead of class for the span elements inside the
-    // create airplane function
-    let crewConfirm = document.querySelectorAll(`data-name[${airplane.name}]`);
-    if (crew <= airplane.crew.length) {
+    // Getting the airplane name in the DOM
+    let airplaneConfirm = document.querySelector(`p[data-airplanename="${airplane.name}"]`);
+    // Getting the crewConfir airplane name in the DOM, to run only on the correct airplane.
+    let crewConfirm = document.querySelector(`span[data-crewname="${airplane.name}"]`);
+    console.log(crewConfirm);
+    if (crew <= airplane.crew.length && crewConfirm.dataset.crewname === airplaneConfirm.dataset.airplanename) {
       crewConfirm.textContent = 'Good!';
       console.log(`${airplane.name} crew is: ${airplane.printCrew}. Requirement Met!`);
-    } else {
+    } else if (crew > airplane.crew.length && crewConfirm.dataset.crewname === airplaneConfirm.dataset.airplanename){
+      crewConfirm.textContent = 'Bad!';
       console.log(`${airplane.name} crew is: ${airplane.printCrew}. Requirement NOT Met!`);
     }
   });
@@ -126,13 +162,32 @@ const meetsCrewReq = (crew) => {
 // Check if airplane meets the range requirements
 const meetsRangeReq = (range) => {
   airplanes.forEach((airplane) => {
-    if (range <= airplane.range) {
+    // Getting the airplane name
+    let airplaneConfirm = document.querySelector(`p[data-airplanename="${airplane.name}"]`);
+    // Getting the flightreq (range) name for the same airplane
+    let flightConfirm = document.querySelector(`span[data-flightname="${airplane.name}"]`);
+    // Checking the range and applying the text
+    if (range <= airplane.range && flightConfirm.dataset.flightname === airplaneConfirm.dataset.airplanename) {
       console.log(`${airplane.name} range is:${airplane.range} Requirement Met!`);
-    } else {
+      flightConfirm.textContent = 'Good!';
+    } else if (range > airplane.range && flightConfirm.dataset.flightname === airplaneConfirm.dataset.airplanename){
+      flightConfirm.textContent = 'Bad!';
       console.log(`${airplane.name} range is:${airplane.range} Requirement NOT Met!`);
     }
   });
 };
+
+const callRangeCheck = e => {
+  // Prevent form submit
+  e.preventDefault();
+  const flightReq = document.getElementById('flightreq').value;
+  meetsRangeReq(flightReq);
+}
+
+// Global variables for the check flight req function:
+const checkFlightReq = document.getElementById('checkflightreq');
+checkFlightReq.addEventListener('click', callRangeCheck);
+
 
 // TESTS:
 // console.log(airplanes);
