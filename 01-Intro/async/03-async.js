1e93265659243eb6988e7ecdb0d09b7f4cb3e6c1
Beto Costa <robertomarx@gmail.com>
2019-05-23 17:14:23 -0300

    more to async 3

--- 01-Intro/async/03-async.js
+++ 01-Intro/async/03-async.js
@@ -1,14 +1,16 @@
 /* CODECADEMY - INTRODUCTION TO JAVASCRIPT
 JAVASCRIPT ASYNC
-Writing async Functions
+Writing async Functions - Handling Dependent Promises
+
 */
 
-/* More information on 03-async.md */
+/* More information on 03-async.md
 
-/*
-15 - Library.js - Copied the function from CodeCademy.
-12 - Writing Async Functions
-68 - The await Operator
+21 - Library.js - Copied the function from CodeCademy.
+34 - Writing Async Functions
+50 - The first function *fixed* to get the value and not a promise
+92 - Handling Dependent Promises
+
 
 */
 
@@ -24,18 +26,117 @@ const shopForBeans = () => {
       let beanType = beanTypes[randomIndex];
       console.log(`2. I bought ${beanType} beans because they were on sale.`)
      resolve(beanType);
+     reject(`I'm so confused, I'll order some food!`);
     }, 1000)
   })
 }
 
 console.log('\n=== Writing async Functions ===\n');
+// First function from Codecademy
+/* This function will not return the ${value} because the function getBeans() will take
+at least one second to run. Since it will have a delay, what we will get is the promise:
+Promise { <pending> }
+We need to change it to an async function and use the await on the value we want.
+See below on line 50 the new function that will work.*/
 
-// First function from Codecademy:
-function getBeans() {
+/* function getBeans() {
   console.log(`1. Heading to the store to buy beans...`);
-  let value = shopForBeans();
+  let value = await shopForBeans();
   console.log(`3. Great! I'm making ${value} beans for dinner tonight!`);
 }
+getBeans(); */
 
+// Using an async function with the await keyword:
+async function getBeans() {
+  console.log(`1. Heading to the store to buy beans...`);
+  let value = await shopForBeans();
+  console.log(`3. Great! I'm making ${value} beans for dinner tonight!`);
+}
 getBeans();
 
+/* Below there is a function created to display the console messages in the correct order.
+It is using an async function and it will return the console message desired acordingly to the
+argument used when calling the function:
+0 = Handling Dependent Promises
+1 = Handling Errors
+2 = Handling Independent Promises
+*/
+
+const returnMessages = (message) => {
+  return new Promise((resolve, reject) => {
+    const messages = ['\n=== Handling Dependent Promises ===\n', '\n=== Handling Errors ===\n', '\n=== Handling Independent Promises ===\n'];
+    setTimeout(() => {
+      if(message === 0) {
+        resolve(console.log(messages[0]));
+      } else if (message === 1) {
+        resolve(console.log(messages[1]));
+      } else if (message === 2) {
+        resolve(console.log(messages[2]));
+      } else {
+        reject(console.log('Not a valid message!'));
+      }
+    }, 1200)
+  })
+}
+
+// Gets the correct message and awaits for the response. The control flow is the setTimeOut (1200) above.
+async function displayMessages(num) {
+  let returnedMessage = await returnMessages(num);
+  return returnedMessage;
+}
+
+// Handling dependent promises
+displayMessages(0);
+
+/* Handling Dependent Promises !! */
+/* Below there are three functions:
+shopForBeans2, soakTheBeans and cookTheBeans
+All functions are from CodeCademy to be used for the codes below their lines.
+shopForBeans is the same function as the above shopForBeans(), only with a new name to simply
+make it work! */
+
+const shopForBeans2 = () => {
+  return new Promise((resolve, reject) => {
+    const beanTypes = ['kidney', 'fava', 'pinto', 'black', 'garbanzo'];
+    setTimeout(()=>{
+      let randomIndex = Math.floor(Math.random() * 5)
+      let beanType = beanTypes[randomIndex];
+      console.log(`Today I bought ${beanType} beans because they were on sale.`)
+     resolve(beanType);
+     reject(`I'm so confused, I'll order some food!`);
+    }, 1500)
+  })
+}
+
+let soakTheBeans = (beanType) => {
+  return new Promise((resolve, reject) => {
+    console.log('Time to soak the beans.')
+   setTimeout(()=>{
+     console.log(`... The ${beanType} beans are softened.`)
+     resolve(true)
+     }, 1500)
+ })
+}
+
+let cookTheBeans = (isSoftened) => {
+ return new Promise((resolve, reject) => {
+   console.log('Time to cook the beans.')
+   setTimeout(()=>{
+     if (isSoftened) {
+       console.log('... The beans are cooked!')
+       resolve('\n\nDinner is served!')
+     }
+   }, 1500)
+ })
+}
+
+/* Handling the Dependent promises for the above functions to work: */
+async function makeBeans() {
+  let type = await shopForBeans2();
+  // console.log(type);
+  let isSoft = await soakTheBeans(type);
+  // console.log(isSoft);
+  let dinner = await cookTheBeans(isSoft);
+  console.log(dinner);
+}
+makeBeans();
--- 01-Intro/async/03-async.md
+++ 01-Intro/async/03-async.md
@@ -2,11 +2,15 @@
 
 ### ASYNC AWAIT
 
-## Writing async Functions
-
-#### Include something else?
+#### Contents:
+- Writing async Functions
+- Handling Dependent Promises
+- Handling Errors
+- Handling Independent Promises
 
 #### Code on 03-async.js
+
+## Writing async Functions
 
 We’ve seen that the `await` keyword halts the execution of an `async` function until a promise is no longer pending. Don’t forget the `await` keyword! It may seem obvious, but this can be a tricky mistake to catch because our function will still run— it just won’t have the desired results.
 
@@ -39,6 +43,64 @@ noAwait(); // Prints: Promise { <pending> }
 yesAwait(); // Prints: Yay, I resolved!
 ```
 
-In the first `async` function, `noAwait()`, we left off the await keyword before `myPromise()`. In the second, `yesAwait()`, we included it. The `noAwait()` function logs `Promise { <pending> }` to the console. Without the `await` keyword, the function execution wasn’t paused. The `console.log()` on the following line was executed before the promise had resolved.
+In the first `async` function, `noAwait()`, we left off the await keyword before `myPromise()`. In the second, `yesAwait()`, we included it. The `noAwait()` function logs `Promise { <pending> }` to the console. Without the `await` keyword, the function execution wasn’t paused. The `console.log()` on the following line *was executed **before** the promise had resolved*.
 
 Remember that the `await` operator returns the resolved value of a promise. When used properly in `yesAwait()`, the variable value was assigned the resolved value of the `myPromise()` promise, whereas in `noAwait()`, value was assigned the promise object itself.
+
+***
+
+## Handling Dependent Promises
+
+The true beauty of `async...await` is when we have a series of asynchronous actions which depend on one another. For example, we may make a network request based on a query to a database. In that case, we would need to wait to make the network request until we had the results from the database. With native promise syntax, we use a chain of `.then()` functions making sure to return correctly each one:
+
+```
+function nativePromiseVersion() {
+    returnsFirstPromise()
+    .then((firstValue) => {
+        console.log(firstValue);
+        return returnsSecondPromise(firstValue);
+    })
+   .then((secondValue) => {
+        console.log(secondValue);
+    });
+}
+```
+
+Let’s break down what’s happening in the `nativePromiseVersion()` function:
+
+- Within our function we use two functions which return promises: `returnsFirstPromise()` and `returnsSecondPromise()`.
+
+- We invoke `returnsFirstPromise()` and ensure that the first promise resolved by using `.then()`.
+
+- In the callback of our first `.then()`, we log the resolved value of the first promise, `firstValue`, and then return `returnsSecondPromise(firstValue)`.
+
+- We use another `.then()` to print the second promise’s resolved value to the console.
+
+Here’s how we’d write an `async` function to accomplish the same thing:
+
+```
+async function asyncAwaitVersion() {
+ let firstValue = await returnsFirstPromise();
+ console.log(firstValue);
+ let secondValue = await returnsSecondPromise(firstValue);
+ console.log(secondValue);
+}
+```
+
+Let’s break down what’s happening in our `asyncAwaitVersion()` function:
+
+- We mark our function as `async`.
+
+- Inside our function, we create a variable `firstValue` assigned `await returnsFirstPromise()`. This means `firstValue` is assigned the resolved value of the awaited promise.
+
+- Next, we log `firstValue` to the console.
+
+- Then, we create a variable `secondValue` assigned to `await returnsSecondPromise(firstValue)`. Therefore, `secondValue` is assigned this promise’s resolved value.
+
+- Finally, we log `secondValue` to the console.
+
+Though using the `async...await` syntax can save us some typing, the length reduction isn’t the main point. Given the two versions of the function, the `async...await` version more closely resembles synchronous code, which helps developers maintain and debug their code. The `async...await` syntax also makes it easy to store and refer to resolved values from promises further back in our chain which is a much more difficult task with native promise syntax. Let’s create some `async` functions with multiple `await` statements!
+
+***
+
+## Handling Errors
