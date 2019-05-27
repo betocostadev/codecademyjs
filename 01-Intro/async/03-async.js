3cff413df93e8e078d41926c80a1ee149be7d0e7
Beto Costa <robertomarx@gmail.com>
2019-05-27 15:34:50 -0300

    async await module done

--- 01-Intro/async/03-async.js
+++ 01-Intro/async/03-async.js
@@ -1,15 +1,18 @@
 /* CODECADEMY - INTRODUCTION TO JAVASCRIPT
 JAVASCRIPT ASYNC
-Writing async Functions - Handling Dependent Promises
-
+Writing async Functions - Handling Dependent Promises - Handling Errors - Handling Independent
+Promises and Await Promise.all()
 */
 
 /* More information on 03-async.md
 
-21 - Library.js - Copied the function from CodeCademy.
-34 - Writing Async Functions
-50 - The first function *fixed* to get the value and not a promise
-92 - Handling Dependent Promises
+24 - Library.js - Copied the function from CodeCademy.
+35 - Writing Async Functions
+51 - The first function *fixed* to get the value and not a promise
+93 - Handling Dependent Promises
+145 - Handling Errors
+191 - Handling Independent Promises
+200 - Await Promise.all()
 
 
 */
@@ -62,9 +65,9 @@ argument used when calling the function:
 2 = Handling Independent Promises
 */
 
-const returnMessages = (message) => {
+const returnMessages = (message, time) => {
   return new Promise((resolve, reject) => {
-    const messages = ['\n=== Handling Dependent Promises ===\n', '\n=== Handling Errors ===\n', '\n=== Handling Independent Promises ===\n'];
+    const messages = ['\n=== Handling Dependent Promises ===\n', '\n=== Handling Errors ===\n', '\n=== Handling Independent Promises ===\n', '\n=== Await Promise.all() ===\n' ];
     setTimeout(() => {
       if(message === 0) {
         resolve(console.log(messages[0]));
@@ -72,21 +75,23 @@ const returnMessages = (message) => {
         resolve(console.log(messages[1]));
       } else if (message === 2) {
         resolve(console.log(messages[2]));
+      } else if (message === 3) {
+        resolve(console.log(messages[3]));
       } else {
         reject(console.log('Not a valid message!'));
       }
-    }, 1200)
+    }, time)
   })
 }
 
 // Gets the correct message and awaits for the response. The control flow is the setTimeOut (1200) above.
-async function displayMessages(num) {
-  let returnedMessage = await returnMessages(num);
+async function displayMessages(num, time) {
+  let returnedMessage = await returnMessages(num, time);
   return returnedMessage;
 }
 
 // Handling dependent promises
-displayMessages(0);
+displayMessages(0, 1200);
 
 /* Handling Dependent Promises !! */
 /* Below there are three functions:
@@ -140,3 +145,123 @@ async function makeBeans() {
   console.log(dinner);
 }
 makeBeans();
+
+/* HANDLING ERRORS */
+displayMessages(1, 5000);
+
+/* Handling Errors module functions */
+// randomSuccess will generate a random number to make a random true/false and make the promise
+// reject or resolve.
+let randomSuccess = () => {
+  let num = Math.random();
+  if (num < 0.5) {
+    return true;
+  } else {
+    return false;
+  }
+};
+
+let cookBeanSouffle = () => {
+  // The setTimeout below was added only to delay the message to log to the console
+  // in the correct order.
+  setTimeout(() => {
+    console.log('Fingers crossed... Putting the Bean Souffle in the oven');
+  }, 5500);
+  return new Promise((resolve, reject) => {
+    setTimeout(()=>{
+      // random resolve, reject
+      let success = randomSuccess();
+      if(success){
+        resolve('Bean Souffle');
+      } else {
+        reject('Dinner is ruined!');
+      }
+    }, 6500);
+  })
+};
+
+async function hostDinnerParty() {
+  try {
+    let dinner = await cookBeanSouffle();
+    console.log(`${dinner} is served!`);
+  } catch (error) {
+    console.log(error);
+    console.log('Ordering a pizza!');
+  }
+}
+
+hostDinnerParty();
+
+/* HANDLING INDEPENDENT PROMISES */
+displayMessages(2, 7000);
+
+/* Independent promises module functions: */
+let cookBeans = () => {
+  return new Promise ((resolve, reject) => {
+   setTimeout(()=>{
+     resolve('beans')
+   }, 8000)
+ })
+}
+
+let steamBroccoli = () => {
+ return new Promise ((resolve, reject) => {
+   setTimeout(()=>{
+     resolve('broccoli')
+   }, 8000)
+ })
+}
+
+let cookRice = () => {
+ return new Promise ((resolve, reject) => {
+   setTimeout(()=>{
+     resolve('rice')
+   }, 8000)
+ })
+}
+
+let bakeChicken = () => {
+ return new Promise ((resolve, reject) => {
+   setTimeout(()=>{
+     resolve('chicken')
+   }, 8000)
+ })
+}
+
+// Handling the independent promises from above:
+async function serveDinner() {
+  let vegetablePromise = steamBroccoli();
+  let starchPromise = cookRice();
+  let proteinPromise = bakeChicken();
+  let sidePromise = cookBeans();
+  /* Notice that, the await was not used above when calling the functions.
+  Instead it is used below, when logging to the console. This means that the functions above
+  will run concurrently. Each one will not need to await for the resolved value of the other
+  to run. Only when we log the results we are awaiting.*/
+  console.log(`Dinner is served. We're having ${await vegetablePromise}, ${await starchPromise}, ${await proteinPromise}, and ${await sidePromise}.`)
+}
+serveDinner();
+
+/* Await Promise.all() */
+displayMessages(3, 9000);
+
+// This function is the same as the above, it uses the same functions we declared above on the
+// previous module.
+// Now we will use a Promise.all() to ask for the promises.
+async function allDinner() {
+  const resultDinner = await Promise.all([steamBroccoli(), cookRice(), bakeChicken(), cookBeans()]);
+  // if we want just to log the results to test the promises:
+  /* for(let i = 0; i < resultDinner.length; i++) {
+    console.log(resultDinner[i]);
+  } */
+  // The advantage of using a Promise.all is that as soon as any of them rejects, the execution stops.
+  console.log(`Using Promise.all()
+Dinner is served. We're having ${resultDinner[0]}, ${resultDinner[1]}, ${resultDinner[2]}, and ${resultDinner[3]}.`)
+};
+
+const callPromAll = () => {
+  setTimeout(() => {
+    allDinner();
+  }, 1000);
+};
+callPromAll();
--- 01-Intro/async/03-async.md
+++ 01-Intro/async/03-async.md
@@ -7,6 +7,7 @@
 - Handling Dependent Promises
 - Handling Errors
 - Handling Independent Promises
+- Await Promise.all()
 
 #### Code on 03-async.js
 
@@ -104,3 +105,85 @@ Though using the `async...await` syntax can save us some typing, the length redu
 ***
 
 ## Handling Errors
+
+When `.catch()` is used with a long promise chain, there is no indication of where in the chain the error was thrown. This can make debugging challenging.
+
+With `async...await`, we use `try...catch` statements for error handling. By using this syntax, not only are we able to handle errors in the same way we do with synchronous code, but we can also catch both synchronous and asynchronous errors. *This makes for easier debugging!*
+
+```
+async function usingTryCatch() {
+ try {
+   let resolveValue = await asyncFunction('thing that will fail');
+   let secondValue = await secondAsyncFunction(resolveValue);
+ } catch (err) {
+   // Catches any errors in the try block
+   console.log(err);
+ }
+}
+
+usingTryCatch();
+```
+
+Remember, since `async` functions return promises we can still use native promise’s `.catch()` with an `async` function:
+
+```
+async function usingPromiseCatch() {
+   let resolveValue = await asyncFunction('thing that will fail');
+}
+
+let rejectedPromise = usingPromiseCatch();
+rejectedPromise.catch((rejectValue) => {
+console.log(rejectValue);
+})
+```
+
+This is sometimes used in the global scope to catch final errors in complex code.
+
+***
+
+## Handling Independent Promises
+
+Remember that `await` halts the execution of our `async` function. This allows us to conveniently write synchronous-style code to handle dependent promises. But what if our `async` function contains multiple promises which are not dependent on the results of one another to execute?
+
+```
+async function waiting() {
+ const firstValue = await firstAsyncThing();
+ const secondValue = await secondAsyncThing();
+ console.log(firstValue, secondValue);
+}
+
+async function concurrent() {
+ const firstPromise = firstAsyncThing();
+ const secondPromise = secondAsyncThing();
+console.log(await firstPromise, await secondPromise);
+}
+```
+
+In the `waiting()` function, we pause our function until the first promise resolves, then we construct the second promise. Once that resolves, we print both resolved values to the console.
+
+In our `concurrent()` function, both promises are constructed without using `await`. We then `await` each of their resolutions to print them to the console.
+
+With our `concurrent()` function both promises’ asynchronous operations can be run simultaneously. If possible, we want to get started on each asynchronous operation as soon as possible! Within our `async` functions we should still take advantage of *concurrency*, the ability to perform asynchronous actions at the same time.
+
+**Note**: if we have multiple truly independent promises that we would like to execute fully in parallel, we must use individual `.then()` functions and avoid halting our execution with `await`.
+
+***
+
+## Await Promise.all()
+
+Another way to take advantage of concurrency when we have multiple promises which can be executed simultaneously is to `await` a `Promise.all()`.
+
+We can pass an array of promises as the argument to `Promise.all()`, and it will return a single promise. This promise will resolve when all of the promises in the argument array have resolved. This promise’s resolve value will be an array containing the resolved values of each promise from the argument array.
+
+```
+async function asyncPromAll() {
+  const resultArray = await Promise.all([asyncTask1(), asyncTask2(), asyncTask3(), asyncTask4()]);
+  for (let i = 0; i<resultArray.length; i++){
+    console.log(resultArray[i]);
+  }
+}
+```
+
+In our above example, we `await` the resolution of a `Promise.all()`. This `Promise.all()` was invoked with an argument array containing four promises (returned from required-in functions). Next, we loop through our `resultArray`, and log each item to the console. The first element in `resultArray` is the resolved value of the `asyncTask1()` promise, the second is the value of the `asyncTask2()` promise, and so on.
+
+`Promise.all()` allows us to take advantage of asynchronicity— each of the four asynchronous tasks can process concurrently. `Promise.all()` also has the benefit of *failing fast*, meaning it won’t wait for the rest of the asynchronous actions to complete once any one has rejected. As soon as the first promise in the array rejects, the promise returned from `Promise.all()` will reject with that reason. As it was when working with native promises, `Promise.all()` is a good choice if multiple asynchronous tasks are all required, but none must wait for any other before executing.
